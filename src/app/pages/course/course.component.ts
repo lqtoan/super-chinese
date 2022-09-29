a1aa9c3d34979a678516b7cbebca7e61ab9df8f9
lqtoan <lqtoan37@gmail.com>
2022-09-29 15:18:59 +0700

    add pipes

--- src/app/pages/course/course.component.ts
+++ src/app/pages/course/course.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { UserProfileService } from '@services/user-profile.service';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.scss']
+  styleUrls: ['./course.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseComponent implements OnInit {
+  constructor(private readonly userProfileService: UserProfileService) {}
 
-  constructor() { }
+  user$ = this.userProfileService.user$;
 
   ngOnInit(): void {
+    this.userProfileService.user$.pipe().subscribe();
+
+    this.getUserProfile();
   }
 
+  getUserProfile() {
+    this.userProfileService.getUserProfile().subscribe();
+  }
 }

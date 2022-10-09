a16751c7b3472fc27d7e4c4ad288b1fe49e9e322
lqtoan <lqtoan37@gmail.com>
2022-10-09 22:55:38 +0700

    update table

--- src/app/pages/dictionary/dictionary-list/dictionary-list.component.ts
+++ src/app/pages/dictionary/dictionary-list/dictionary-list.component.ts
@@ -43,16 +43,13 @@ export class DictionaryListComponent implements OnInit {
       sortFn: this.sortPinyin,
       sortPriority: false,
     },
+    { label: 'DICTIONARY.DEFINE', field: 'define', width: '16rem' },
     {
       label: 'DICTIONARY.CHINA_VIETNAM_WORD',
       field: 'chinaVietnamWord',
       cellType: 'titlecase',
       width: '8rem',
-      // sortOrder: 'ascend',
-      // sortFn: this.sortChinaVietnamWord,
-      // sortPriority: false,
     },
-    { label: 'DICTIONARY.DEFINE', field: 'define', width: '16rem' },
     {
       label: 'HSK',
       field: 'hsk',

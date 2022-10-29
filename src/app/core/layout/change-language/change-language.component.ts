825b40c7052150fffb6a568902bae1e346f3eb21
lqtoan <lqtoan37@gmail.com>
2022-10-29 22:27:25 +0700

    update ui

--- src/app/core/layout/change-language/change-language.component.ts
+++ src/app/core/layout/change-language/change-language.component.ts
@@ -8,13 +8,21 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ChangeLanguageComponent implements OnInit {
   currentLanguage = localStorage.getItem('language');
+  languages = this.translateService.getLangs();
 
   constructor(private readonly translateService: TranslateService) {}
 
   ngOnInit(): void {}
 
-  changeLanguage(event: any) {
-    this.translateService.use(event.target.value);
-    localStorage.setItem('language', event.target.value);
+  // changeLanguage(event: any) {
+  //   console.log(event);
+
+  //   this.translateService.use(event.target.value);
+  //   localStorage.setItem('language', event.target.value);
+  // }
+
+  changeLanguage(language: string) {
+    this.translateService.use(language);
+    localStorage.setItem('language', language);
   }
 }

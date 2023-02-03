dceef45f8714a74350739499a1fcb8c99551ed3c
lqtoan <lqtoan37@gmail.com>
2023-02-03 22:31:54 +0700

    reload audio

--- src/app/core/inteceptors/index.ts
+++ src/app/core/inteceptors/index.ts
@@ -1,16 +1,10 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NoCacheHeadersInterceptor } from './no-cache-interceptor';
 import { BearerTokenInterceptor } from './bearer-interceptor';
 
 export const interceptorProviders = [
   {
     provide: HTTP_INTERCEPTORS,
     useClass: BearerTokenInterceptor,
-    multi: true,
-  },
-  {
-    provide: HTTP_INTERCEPTORS,
-    useClass: NoCacheHeadersInterceptor,
     multi: true,
   },
 ];

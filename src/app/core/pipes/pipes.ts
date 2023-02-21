5ceff313dcf9edd093cafa88991cad4f5307f807
lqtoan <lqtoan37@gmail.com>
2023-02-21 16:27:00 +0700

    init nx

--- src/app/core/pipes/pipes.ts
+++ src/app/core/pipes/pipes.ts
@@ -3,7 +3,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'formatPrice' })
 export class FormatPricePipe implements PipeTransform {
   transform(value: string): string {
-    if (!value) { return '0'; }
+    if (!value) {
+      return '0';
+    }
     value = value.toString().replace(/[\,]+/g, '');
     value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     return value.concat(' ₫');
--- src/app/pages/dictionary/dictionary-list/dictionary-list.component.ts
+++ src/app/pages/dictionary/dictionary-list/dictionary-list.component.ts
@@ -1,10 +1,5 @@
 import { FilterType } from '@enums/dictionary.enum';
-import { TranslateService } from '@ngx-translate/core';
-import { LanguageService } from './../../../core/layout/change-language/language.service';
-import { DictionaryStore } from './../dictionary.store';
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { NzModalService } from 'ng-zorro-antd/modal';
-import { NzI18nService, vi_VN, en_US, zh_CN } from 'ng-zorro-antd/i18n';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dictionary-list',

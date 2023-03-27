c53c721e189e9c107a13007ed52a0b8a7aee870a
lqtoan <lqtoan37@gmail.com>
2023-03-27 18:16:26 +0700

    fix cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,5 +14,5 @@ export const environment = {
   },
   authApi: 'https://hsk-1.us.auth0.com',
   api: 'https://super-chinese.cyclic.app/api/',
-  webSocket: 'https://super-chinese-api.onrender.com',
+  webSocket: 'https://super-chinese.cyclic.app',
 };
